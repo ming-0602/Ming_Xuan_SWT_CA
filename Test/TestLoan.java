69bff68e3a5b11b9c6baf39cd7eb83947ee60003
mingxuan8833 <mingxuan8833@gmail.com>
2023-03-28 07:30:43 +0800

    Working on Last Question

---
+++ Test/TestLoan.java
@@ -0,0 +1,142 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestLoan {
+    //Test Period
+    @Test
+    public void testperiod(){
+        Loan loan = new Loan(500, 3);
+        assertEquals(3, loan.getPeriod());
+    }
+    @Test
+    public void testAmount(){
+        Loan loan = new Loan(500, 3);
+        assertEquals(500,loan.getAmount());
+    }
+    @Test
+    public void testRate(){
+        Loan loan = new Loan(500, 3);
+        assertEquals(10, loan.getRate());
+    }
+
+    @Test
+    public void testgetMonthlyPayment(){
+        Loan loan = new Loan(500, 1);
+        assertEquals(43.957943615004936, loan.getMonthlyPayment());
+    }
+
+    @Test
+    public void testgetTotalPayment(){
+        Loan loan = new Loan(500, 1);
+        assertEquals(527.4953233800593, loan.getTotalPayment());
+    }
+
+
+    @Test
+    public void PositiveTest1(){
+        Loan loan = new Loan(500,1);
+        assertEquals(10,loan.getRate());
+    }
+
+    @Test
+    public void PositiveTest2(){
+        Loan loan = new Loan(5000,3);
+        assertEquals(10,loan.getRate());
+    }
+
+    @Test
+    public void PositiveTest3(){
+        Loan loan = new Loan(5001,1);
+        assertEquals(8,loan.getRate());
+    }
+
+    @Test
+    public void PositiveTest4(){
+        Loan loan = new Loan(10000,3);
+        assertEquals(8,loan.getRate());
+    }
+
+    @Test
+    public void PositiveTest5(){
+        Loan loan = new Loan(500,4);
+        assertEquals(6,loan.getRate());
+    }
+
+    @Test
+    public void PositiveTest6(){
+        Loan loan = new Loan(5000,5);
+        assertEquals(6,loan.getRate());
+    }
+
+    @Test
+    public void PositiveTest7(){
+        Loan loan = new Loan(5001,4);
+        assertEquals(5,loan.getRate());
+    }
+
+    @Test
+    public void PositiveTest8(){
+        Loan loan = new Loan(10000,5);
+        assertEquals(5,loan.getRate());
+    }
+
+    @Test()
+    public void NagativeTest1() throws IllegalArgumentException{
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Loan(499,1);
+        });
+    }
+
+    @Test()
+    public void NagativeTest2() throws IllegalArgumentException{
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Loan(10001,1);
+
+        });
+    }
+
+    @Test()
+    public void NagativeTest3() throws IllegalArgumentException{
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Loan(0,0);
+        });
+    }
+
+    @Test()
+    public void NagativeTest4() throws IllegalArgumentException{
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Loan(500,0);
+        });
+    }
+
+    @Test()
+    public void NagativeTest5() throws IllegalArgumentException{
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Loan(500,6);
+        });
+    }
+
+    @Test()
+    public void NagativeTest6() throws IllegalArgumentException{
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Loan(Double.parseDouble("Abcd"),0);
+        });
+    }
+
+    @Test()
+    public void NagativeTest7() throws IllegalArgumentException{
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Loan(500,Integer.parseInt("AbcD"));
+        });
+    }
+
+    @Test
+    public void testsetRate(){
+        Loan loan = new Loan(500, 2);
+        assertEquals(10,loan.getRate());
+    }
+
+
+
+}
